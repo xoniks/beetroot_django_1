2b37729801b695a764ec10b8148bb2f9a22f8226
Egezon Baruti <63745689+xoniks@users.noreply.github.com>
2023-12-23 20:33:24 +0000

    about added

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import homePageView
+from .views import HomePageView, AboutPageview
 
 
 urlpatterns =[
-    path('',homePageView,name='home'),
+    path('',HomePageView.as_view(),name='home'),
+    path('about/',AboutPageview.as_view(),name = 'about')
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
 
 
-def homePageView(request):
-    return HttpResponse('Hello world from django!')
+class AboutPageview(TemplateView):
+    template_name = 'about.html'
